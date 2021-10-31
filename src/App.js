7a9ac586716bbb294099f43a43730965257d8fb1
Jasmine Ryon <jryon12@priorypride.net>
2021-10-31 15:02:50 -0700

    turning app.js into class component instead to use redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import React, {Component} from 'react'
 import './App.css';
 import EventCards from './containers/EventCards'
 import EventList from './components/EventList'
 import Typography from '@mui/material/Typography'
+class App extends Component {
 
-function App() {
-  return (
-    <>
-      <header className="App-header">
-        <Typography variant="h1">Event List</Typography>
-        <hr />
-      </header>
-      <EventList />
-      <EventCards />
-    </>
-  );
+  componentDidMount() {
+    console.log("Going to fetch months next")
+  }
+
+  // componentDidMount() {
+  //   console.log("Going to fetch events next")
+  // }
+
+  render() {
+    return (
+      <>
+        <header className="App-header">
+          <Typography variant="h1">Event List</Typography>
+          <hr />
+        </header>
+        <EventList />
+        <EventCards />
+      </>
+    );
+  }
 }
 
 export default App;
