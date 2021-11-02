58f6451b06d67a09ca7c5b9fb2d3a93f197aafdc
Jasmine Ryon <jryon12@priorypride.net>
2021-11-02 09:54:35 -0700

    creates sendSignup fetch in userActionCreator

--- src/redux/actions/userActionCreator.js
+++ src/redux/actions/userActionCreator.js
@@ -1,3 +1,5 @@
+const api_url = 'http://localhost:3000/api/v1/users'
+
 export const toggleSignup = () => ({type: "TOGGLE_SIGNUP"})
 
 export const handleLoginFormChange = (e) => ({
@@ -10,3 +12,23 @@ export const handleLoginFormChange = (e) => ({
     // passing that payload over to userReducer
 // change name & value input field for password, and putting it into payload
 // reducer will update state
+
+export const sendSignup = (userData) => {
+    return dispatch => {
+        fetch(api_url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        })
+        .then(resp => resp.json())
+        .then(resp => dispatch({
+            type: "SET_USER",
+            payload: {user: resp.user}
+            // payload: resp.user
+        }))
+    }
+}
+// don't want to just send back user data, making it nested
+// also want to send back jwt token
